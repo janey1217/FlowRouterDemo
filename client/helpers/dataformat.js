5f001d7584e11bec49bfd0aab995db9070a64aa7
简彦敏 <792074052@qq.com>
2015-10-11 19:29:10 +0800

    管理编辑

--- client/helpers/dataformat.js
+++ client/helpers/dataformat.js
@@ -3,10 +3,11 @@
  */
 
 UI.registerHelper('dateFormate', function(thing) {
-  var str=new Date();
-  str=thing;
+  var str=new Date(thing);
+  //str=thing;
   //return str;
-  return  str.toLocaleDateString()+" "+str.toLocaleTimeString();
+  return moment(str).format("YYYY-MM-DD  HH:mm:ss");
+  //return  str.toLocaleDateString()+" "+str.toLocaleTimeString();
 });
 
 UI.registerHelper("userIdFormate", function (userId) {
--- client/template/managediscussion/manage-discussion-item.js
+++ client/template/managediscussion/manage-discussion-item.js
@@ -5,13 +5,20 @@ Template.manageDiscussionItem.events({
   "click button.viewBtn ": function (e, template) {
    console.log(template.data._id);
    // e.preventDefault();
-    console.log("hello viewBtn");
+    //console.log("hello viewBtn");
+      FlowRouter.go("singleDisc", {discId : template.data._id});
+
   },
 
   "click button.editorBtn ": function (e, template) {
-    // console.log(template.data._id);
+     console.log(template.data._id+"  "+ template.data.subject+" "+template.data.content);
      // e.preventDefault();
      // console.log("hello");
+      var editDisc={_id:template.data._id, subject:template.data.subject, content:template.data.content };
+      console.log(editDisc);
+      Session.set("editDisc",editDisc);
+
+      $(".modalBtn").click();
     template.$(".viewBtn").click();
     },
 
